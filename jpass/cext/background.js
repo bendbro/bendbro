88862d12f6ec9b91316989c6eea6b5358bd9e920
ben <bendbro@gmail.com>
2015-10-11 15:59:24 -0500

    tuff

--- jpass/cext/background.js
+++ jpass/cext/background.js
@@ -15,8 +15,47 @@ function loadScript(path, callback) {
     head.appendChild(script);
 }
 
+function findLoginFields(candidates, node) {
+	var passwordIdentifiers = ["password","pw","pword","pass"];
+	
+	var attrs = node.attributes;
+	if(attrs != null) {
+		for(var i = attrs.length - 1; i >= 0; i--) {
+			var contents = attrs[i].name;
+			for(piIndex in passwordIdentifiers) {
+				if(contents.indexOf(passwordIdentifiers[piIndex]) > -1) {
+					candidates.passwords.push(contents);
+				}
+			}
+		}
+	}
+
+	for(childNode in node.childNodes) {
+		findLoginFields(candidates, childNode);
+	}
+	
+	return candidates;
+}
+
 var client = 1337;
+var data = [];
+var model = null;
+var masterPassword = null;
 loadScript("resources/dropbox.min.js",function() {
+loadScript("resources/ModelView.js", function() {
 	client = new Dropbox.Client({key:"q29ccmrl21l9e71"});
 	client.authDriver(new Dropbox.AuthDriver.ChromeExtension({receiverPath: "/resources/chrome_oauth_receiver.html"}));
-});
+	model = new JsObjectModel(data);
+	
+	chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+	  chrome.tabs.executeScript(tab.id,{
+		code: findLoginFields.toString() + '\nalert(JSON.stringify(findLoginFields({passwords:[]},document.body)));' + 'document.body.style.backgroundColor="red";'
+	  });
+	});
+
+	chrome.tabs.onCreated.addListener(function(tabId, changeInfo, tab) {
+	  chrome.tabs.executeScript(tab.id,{
+		code: findLoginFields.toString() + '\nalert(JSON.stringify(findLoginFields({passwords:[]},document.body)));' + 'document.body.style.backgroundColor="red";'
+	  });
+	});
+})});
--- jpass/cext/popup.js
+++ jpass/cext/popup.js
@@ -1,36 +1,107 @@
-function loadScript(path, callback) {
-	// Adding the script tag to the head as suggested before
-    var head = document.getElementsByTagName('head')[0];
-    var script = document.createElement('script');
-    script.type = 'text/javascript';
+function main(clientReference, dataReference, modelReference, masterPasswordReference) {
+	var client = clientReference.get();
+	var dataFile = null; 
+	var view = new LabelledTableView(document.getElementById("credentials"),["domain","username","password"], modelReference.get());
+	
+	var dataFileUpdateNotify = function() {
+		dataReference.set(JSON.parse(sjcl.decrypt(masterPasswordReference.get(), dataFile)));
+		modelReference.get().applyData(dataReference.get());
+		view.update(modelReference.get());
+	};
+	
+	client.authenticate(function(error, client) {
+		client.readFile("credentials.json",function(error, fileData) {
+			if(!error) {
+				dataFile = fileData;
+				if(dataFile != null && masterPasswordReference.get() != null) {
+					dataFileUpdateNotify.call();
+				}
+			}
+		});
+	});
+	
+	function unlock() {
+		masterPasswordReference.set(document.getElementById("masterPassword").value);
+		if(dataFile != null && masterPasswordReference.get() != null) {
+			dataFileUpdateNotify.call();
+		}
+	}
+	
+	document.getElementById("unlockTrigger").addEventListener("click",unlock);
+	document.getElementById("masterPassword").addEventListener("keydown", function(e) {
+		if (!e) {
+			var e = window.event; 
+		}
+		// Enter is pressed
+		if (e.keyCode == 13) { 
+			e.preventDefault(); // sometimes useful
+			$('#masterPasswordInput').modal('hide');
+			unlock();
+		}
+	}, false);
 
-	var chromeId = chrome.runtime.id;
-    //script.src = "chrome-extension://" + chrome.runtime.id + "/" + path;
-	script.src = path;
-
-    script.onreadystatechange = callback;
-    script.onload = callback;
-
-    // Fire the loading
-    head.appendChild(script);
+	if(masterPasswordReference.get() == null) {
+		$('#masterPasswordInput').modal('show');
+	}
+	
+	var submit = document.getElementById("submitchanges");
+	submit.onclick = function() {
+		var saved = true;
+		for(entryIndex in modelReference.get().innerData()){
+			var entry = modelReference.get().innerData()[entryIndex];
+			for(property in entry.innerData()) {
+				if(entry.innerData()[property].saved == false) {
+					saved = false;
+					entry.innerData()[property].saved = true;
+				}
+			}
+		}
+		if(saved == false) {
+			client.writeFile("credentials.json",sjcl.encrypt(masterPasswordReference.get(),JSON.stringify(dataReference.get())),function(error,stat) {});
+		}
+	}
+	
+	var newdata = document.getElementById("addcredential");
+	newdata.onclick = function() {
+		dataReference.get().push({domain:"domain",username:"username", password:"password"});
+		modelReference.get().applyData(dataReference.get());
+		view.update(modelReference.get());
+	};
 }
 
 chrome.runtime.getBackgroundPage(function(backgroundWindow) {
-	var client = backgroundWindow.client;
-	
-	loadScript("resources/dropbox.min.js", function() {
-		client.authenticate(function(error,client){
-			if(error) {
-				alert("auth error!");
-			} else {
-				client.readFile("credentials.json", function(error,data) {
-					if(error) {
-						alert("read error!");
-					} else {
-						alert(data);
-					}
-				});
+	main(
+		{
+			get : function() {
+				return backgroundWindow.client;
+			},
+			set : function(value) {
+				return backgroundWindow.client = value;
 			}
-		});	
-	});
+		},
+		{
+			get : function() {
+				return backgroundWindow.data;
+			},
+			set : function(value) {
+				return backgroundWindow.data = value;
+			}
+		},
+		{
+			get : function() {
+				return backgroundWindow.model;
+			},
+			set : function(value) {
+				return backgroundWindow.model = value;
+			}
+		},
+		{
+			get : function() {
+				return backgroundWindow.masterPassword;
+			},
+			set : function(value) {
+				return backgroundWindow.masterPassword = value;
+			}
+		}
+	);
 });
