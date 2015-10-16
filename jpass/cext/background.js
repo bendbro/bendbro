2a796f6d7994d7372802596d481ee0d0ab009d67
ben <bendbro@gmail.com>
2015-10-16 01:31:51 -0500

    Password injection basic functionality now working

--- jpass/cext/background.js
+++ jpass/cext/background.js
@@ -15,6 +15,26 @@ function loadScript(path, callback) {
     head.appendChild(script);
 }
 
+function extractDomain(url) {
+    var domain;
+    //find & remove protocol (http, ftp, etc.) and get domain
+    if (url.indexOf("://") > -1) {
+        domain = url.split('/')[2];
+    }
+    else {
+        domain = url.split('/')[0];
+    }
+
+    //find & remove port number
+    domain = domain.split(':')[0];
+
+    return domain;
+}
+
+function extractRootDomain(domain) {
+	var domainSplit = domain.split('.');
+	return domainSplit[domainSplit.length-2] + "." + domainSplit[domainSplit.length-1];
+}
 
 function findPasswordFields(candidates, node) {
 	var nodel = node;
@@ -63,17 +83,15 @@ function findTextField(node, last, depth) {
 	}
 	
 	if(hits > 0) {
-		return {node:nodel, depth:depth};
+		return {node:nodel, depth:depth, matching:hits};
 	} else {
-		var minDistance = 999999;
-		var minNode = null;
+		var min = {depth:999999,matching:0};
 		for(childNodeIndex in nodel.childNodes) {
 			var cNode = nodel.childNodes[childNodeIndex];
 			if(cNode != last) {
 				var cFound = findTextField(cNode, nodel, depth+1);
-				if(cFound.depth < minDistance) {
-					minDistance = cFound.depth;
-					minNode = cNode;
+				if(cFound.depth < min.depth || (cFound.depth == min.depth && cFound.matching > min.matching)) {
+					min = cFound;
 				}
 			}
 		}
@@ -81,17 +99,16 @@ function findTextField(node, last, depth) {
 		var pNode = nodel.parentNode;
 		if(pNode != last) {
 			var pFound = findTextField(pNode, nodel, depth+1);
-			if(pFound.depth < minDistance) {
-				minDistance = pFound.depth;
-				minNode = pNode;
+			if(pFound.depth < min.depth || (pFound.depth == min.depth && pFound.matching > min.matching)) {
+				min = pFound;
 			}
 		}
 		
-		return {node:minNode, depth:minDistance};
+		return min;
 	}
 }
 
-function findCredentials() {
+function injectMain(credentials) {
 	var candidates = [];
 	findPasswordFields(candidates,document.body);
 	
@@ -104,12 +121,13 @@ function findCredentials() {
 			maxCandidate = candidate;
 		}
 	}
-
-	maxCandidate.password.k.style.backgroundColor = "red";
-
-	var pFound = findTextField(maxCandidate.password.k, maxCandidate.password.k, 0);
+	var pFound = findTextField(maxCandidate.password.k.parentNode, maxCandidate.password.k, 0);
 	
-	return {text:pFound.node,password:maxCandidate.password.k};
+	maxCandidate.password.k.style.backgroundColor = "#3F51B5";
+	pFound.node.style.backgroundColor = "#2196F3";
+	
+	pFound.node.value = credentials.username;
+	maxCandidate.password.k.value = credentials.password;
 }
 
 var client = 1337;
@@ -122,15 +140,29 @@ loadScript("resources/ModelView.js", function() {
 	client.authDriver(new Dropbox.AuthDriver.ChromeExtension({receiverPath: "/resources/chrome_oauth_receiver.html"}));
 	model = new JsObjectModel(data);
 	
+	//using chrome.tabs api
 	chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-	  chrome.tabs.executeScript(tab.id,{
-		code: findTextField.toString() + '\n' + findPasswordFields.toString() + '\n' + findCredentials.toString() + '\n' + 'console.log("here we go");console.log(findCredentials());'
-	  });
-	});
-
-	chrome.tabs.onCreated.addListener(function(tabId, changeInfo, tab) {
-	  chrome.tabs.executeScript(tab.id,{
-		code: findTextField.toString() + '\n' + findPasswordFields.toString() + '\n' + findCredentials.toString() + '\n' + 'console.log("here we go");console.log(findCredentials());'
-	  });
+	  console.log("a tab was updated");
+		
+	  var tabRootDomain = extractRootDomain(extractDomain(tab.url));
+	  var matchingCredentials = null;
+	  for(entryIndex in data) {
+		  var entry = data[entryIndex];
+		  if(entry.domain.indexOf(tabRootDomain) > -1) {
+			  matchingCredentials = entry;
+		  }
+	  }
+	  
+	  if(matchingCredentials != null) {
+		  console.log({action:"injecting script.",tab:tab,credentials:matchingCredentials});
+		  chrome.tabs.executeScript(tab.id,{
+			code: 
+				'console.log("executing injected script")' + '\n' +
+				findTextField.toString() + '\n' + 
+				findPasswordFields.toString() + '\n' + 
+				injectMain.toString() + '\n' + 
+				'injectMain(' + JSON.stringify(matchingCredentials) + ');' + '\n'
+		  });
+	  }
 	});
 })});
